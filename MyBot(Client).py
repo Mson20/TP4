4ce75c41b395342f4fb630cc391f09e6ee6824ea
Saamson <stephengeorgee@et.esiea.fr>
2022-04-23 20:52:23 +0200

    fifth commit

--- MyBot(Client).py
+++ MyBot(Client).py
@@ -14,4 +14,13 @@ async def on_member_join(member):
     general_channel: discord.TextChannel = client.get_channel(959348198969843735)
     await general_channel.send(content=f"Bienvenue sur le serveur {member.display_name} !")
 
-client.run("OTU5MzQ5MjY0ODMwNTgyODA0.YkaloA.z-kcD2GSZMFAWuvy9w9XHD6Ggcg")
+@client.event
+async def on_message(message):
+    print(message.content)
+
+@client.event
+async def on_message(message):
+    if message.content == "Ping":
+        await message.channel.send("Pong")
+
+client.run("OTU5MzQ5MjY0ODMwNTgyODA0.YkaloA.3HXqDRA8L0cMkdDHFr2srQ1roXs")
